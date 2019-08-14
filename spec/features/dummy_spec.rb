b3ab709de6cc94e250c12f3a9d9470cab57710a6
Jelani Woods <jelani@firstdraft.com>
2019-08-14 15:52:02 -0500

    wip

--- spec/features/dummy_spec.rb
+++ spec/features/dummy_spec.rb
@@ -5,3 +5,102 @@ describe "This project" do
     expect(1).to eq(1)
   end
 end
+
+
+describe "" do
+  it "" do
+    # page.should have_content('Like')
+    page.select("")
+  end
+end
+
+describe "/photos/[SOME ID]" do
+  it "automatically associates photo with signed in user", points: 2 do
+    first_user = User.new
+    first_user.password = "password"
+    first_user.username = "alice"
+    first_user.save
+
+    photo = Photo.new
+    photo.image = "https://some.test/image-#{Time.now.to_i}.jpg"
+    photo.caption = "Some test caption #{Time.now.to_i}"
+    photo.owner_id = first_user.id
+    photo.save
+
+    visit "/sign_in"
+    
+    within(:css, "form") do
+      fill_in "Username", with: first_user.username
+      fill_in "Password", with: first_user.password
+      click_on "Sign in"
+    end
+    
+    visit "/photos/#{photo.id}"
+
+
+    expect(page).to have_text("Update photo")
+  end
+end
+
+describe "/photos/[ID] — Add comment form" do
+  it "automatically associates comment with signed in user", points: 2 do
+    first_user = User.new
+    first_user.password = "password"
+    first_user.username = "alice"
+    first_user.save
+
+    photo = Photo.new
+    photo.image = "https://some.test/image-#{Time.now.to_i}.jpg"
+    photo.caption = "Some test caption #{Time.now.to_i}"
+    photo.owner_id = first_user.id
+    photo.save
+
+    visit "/sign_in"
+    
+    within(:css, "form") do
+      fill_in "Username", with: first_user.username
+      fill_in "Password", with: first_user.password
+      click_on "Sign in"
+    end
+
+    test_comment = "Hey, what a nice app you're building!"
+
+    visit "/photos/#{photo.id}"
+
+    fill_in "Comment", with: test_comment
+
+    click_on "Add comment"
+
+    added_comment = Comment.where({ :author_id => first_user.id, :photo_id => photo.id, :body => test_comment }).at(0)
+
+    expect(added_comment).to_not be_nil
+  end
+end
+
+# describe "/photos/[ID]" do
+#   it "shows edit form when photo belongs to signed in user", points: 1 do
+
+#     image = "https://some.test/image-#{Time.now.to_i}.jpg"
+#     test_caption = "Some test caption #{Time.now.to_i}"
+
+#     user = User.new
+#     user.username = "BagelFace"
+#     user.save
+    
+#     photo = Photo.new
+#     photo.image = image
+#     photo.caption = test_caption
+#     photo.owner_id = user.id
+#     photo.save
+
+#     log_in user
+
+#     visit "/photos/#{photo.id}"
+
+#     page.find_button("Update photo").click
+
+
+
+
+#   end
+# end
